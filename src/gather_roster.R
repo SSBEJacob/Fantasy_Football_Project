a7e2f4dbb42600d68274d8c45b0b00ceeee1da5d
SSBEJacob <71368829+SSBEJacob@users.noreply.github.com>
2021-12-13 16:10:06 -0800

    Update gather_roster.R

--- src/gather_roster.R
+++ src/gather_roster.R
@@ -1,7 +1,7 @@
 library(nflfastR)
 #num_seasons lets you decide how many years worth of data you would like to use.
 #maximum is 20 and only takes integers.
-num_seasons=20
+num_seasons=3
 start_season=2022-num_seasons
 future::plan('multisession')
 rosters <- nflfastR::fast_scraper_roster(start_season:2021)
